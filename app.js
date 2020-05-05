d441255e4c5ae3f646157ea4484d089f40c7b500
francescolucania <akozlov@autospot.ru>
2020-05-05 18:35:55 +0300

    master create & conected Mongo DB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -8,8 +9,19 @@ const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const app = express();
+const keys = require('./config/keys');
 
 // services
+mongoose.connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+    .then(() => {
+        console.log('MONGO DB connected')
+    })
+    .catch(error => console.log(error));
+
 app.use(morgan('dev'));
 
 app.use(cors('cors'));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://francescolucaniacom:fSUaQRaQm-6Fi2c@cluster0-uqq3t.mongodb.net/test?retryWrites=true&w=majority'
+}
